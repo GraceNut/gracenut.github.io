3a0613ad08c877f1d71dd35099aeaa2e88569418
Haley Gerstenberg <gracenut96@gmail.com>
2019-05-02 17:38:41 -0500

    blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-//import { Link } from "gatsby"
+import { Link } from "gatsby"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -10,23 +10,28 @@ import "../components/page-styles/index.scss"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <p class="bio">
-      Hi, I’m Haley Gerstenberg. If you want to get to know me a bit, this is the place to do it.  
-      I do a variety of things. I’m a lifelong bad writer, trying to be a consistent one. If you’d like to say hi, 
-      feel free to reach out on <a href="https://twitter.com/imhaleyfyi">Twitter</a>.
-    </p>
+    <div class="bio">
+      <h2>Intro</h2>
+      <p>
+        Hi, I’m Haley Gerstenberg. If you want to get to know me a bit, this is the place to do it.  
+        I do a variety of things. I’m a lifelong bad writer, trying to be a consistent one. If you’d like to say hi, 
+        feel free to reach out on <a href="https://twitter.com/imhaleyfyi">Twitter</a>.
+      </p>
+    </div>
 
     <div class="posts">
       <h2>Writing</h2>
       <ul>
         { data.allMarkdownRemark.edges.map(({ node }) => (
           <li key={ node.id } className="post">
-            <h3 className="title">{ node.frontmatter.title }</h3>
-            <p className="excerpt">{ node.excerpt }</p>
-            <div class="info">
-              <span className="date">{ node.frontmatter.date }</span>
-              <span className="readtime">read time:&nbsp;{ node.timeToRead }min</span>
-            </div>
+            <Link to={ node.fields.slug } className="post-link">
+              <h3 className="title">{ node.frontmatter.title }</h3>
+              <p className="excerpt">{ node.excerpt }</p>
+              <div class="info">
+                <span className="date">{ node.frontmatter.date }</span>
+                <span className="readtime">read time:&nbsp;{ node.timeToRead }min</span>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
@@ -45,6 +50,9 @@ query HomePageQuery{
         frontmatter {
           title
           date
+        }
+        fields {
+          slug
         }
         excerpt
         timeToRead
---
+++ src/writing/test-one.md
@@ -0,0 +1,7 @@
+---
+title: "post #1"
+date: "2019-05-01"
+author: "haley g"
+---
+
+post number one yeh
